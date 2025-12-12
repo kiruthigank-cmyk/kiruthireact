6b56cce5207249f199c637b61990787b05eb1801
kiruthigank-cmyk <kiruthigauthayakumar@gmail.com>
2025-12-12 10:54:04 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+class App  extends React.Component
+{
+ render()
+ {
+  return(<>
+          <h1>Welcome</h1>
+          <h1>Hello</h1>
+          <h1>Hi</h1>
+          <h1>Haii</h1>     
+                    <h1>Welcome</h1>
+<p>ariatur ad esse qui sit commodo fugiat ullamco dolor. Officia est sunt incididunt enim sunt ad labore pariatur duis magna voluptate fugiat sunt ullamco. Et quis sunt aliqua sit elit. Ut dolor elit dolore cillum excepteur ea ad nulla fugiat.
+<br></br>
+Id deserunt incididunt reprehenderit proident proident adipisicing est ut minim ex laborum commodo ad officia. Minim qui ut aute pariatur sint labore minim. Exercitation cillum enim commodo anim sint irure consectetur commodo sit deserunt. Ad qui tempor in eiusmod qui eu ad eu. Sunt commodo cupidatat ipsum culpa reprehenderit amet anim. Laboris ut voluptate magna esse. Amet id aliquip voluptate ea anim irure culpa magna aliqua.
+<br></br>
+Velit officia sit in eiusmod exercitation culpa minim sunt deserunt. Mollit sint pariatur exercitation amet qui eu. Nisi velit cupidatat aliqua cupidatat nostrud sit. Quis incididunt minim consectetur velit minim ut. Laboris esse esse ad tempor aliquip nulla do aliqua id proident ipsum voluptate veniam consectetur.<br></br>
+</p>
+  </>
+
+  )
+ }
+}
+export default App
+/*
+function App()
+{
+  return(
+    <> 
+    <h1>Welcome Kiruthiga</h1>
+    <h1>Welcome Kiruthiga</h1>
+    <h1>Welcome Kiruthiga</h1>
+    </>
+  )
+}
+export default App
+/*
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+*/
